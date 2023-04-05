22871065b88fe166bd9189fd6772db197488333c
marques <marques.python@gmail.com>
2023-04-04 21:36:25 -0300

    commit 01

--- apps/cliente/views.py
+++ apps/cliente/views.py
@@ -9,30 +9,58 @@ def home(request):
     return render(request, 'home.html')
 
 def convenios(request):
-  return render(request, 'cliente/convenios.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/convenios.html')
+  else:
+    return render(request, 'cliente/convenios.html')
 
 def financeiro(request):
-  return render(request, 'cliente/financeiro.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/financeiro.html')
+  else:
+    return render(request, 'cliente/financeiro.html')
 
 def novaConsulta(request):
-  return render(request, 'cliente/nova_consulta.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/nova_consulta.html')
+  else:
+    return render(request, 'cliente/nova_consulta.html')
 
 def novoExame(request):
-  return render(request, 'cliente/novo_exame.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/novo_exame.html')
+  else:
+    return render(request, 'cliente/novo_exame.html')
 
 ################################################################## MINHAS GUIAS
 def mgSolicitadas(request):
-  return render(request, 'cliente/mg1-solicitadas.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/mg1-solicitadas.html')
+  else:
+    return render(request, 'cliente/mg1-solicitadas.html')
+
 def mgAprovadas(request):
-  return render(request, 'cliente/mg2-aprovadas.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/mg2-aprovadas.html')
+  else:
+    return render(request, 'cliente/mg2-aprovadas.html')
 
 def mgFaturadas(request):
-  return render(request, 'cliente/mg3-faturadas.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/mg3-faturadas.html')
+  else:
+    return render(request, 'cliente/mg3-faturadas.html')
 
 def mgAuditadas(request):
-  return render(request, 'cliente/mg4-auditadas.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/mg4-auditadas.html')
+  else:
+    return render(request, 'cliente/mg4-auditadas.html')
 
 def mgCanceladas(request):
-  return render(request, 'cliente/mg5-canceladas.html')
+  if request.device['is_mobile']:
+    return render(request, 'cliente-mob/mg5-canceladas.html')
+  else:
+    return render(request, 'cliente/mg5-canceladas.html')
 
 ###############################################################################
