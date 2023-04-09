b1c24dd2c5cbfe6d12d686630805ec9f437da8dd
marques <marques.python@gmail.com>
2023-04-09 15:17:46 -0300

    commit

--- apps/cliente/views.py
+++ apps/cliente/views.py
@@ -10,24 +10,32 @@ def home(request):
   else:
     return render(request, 'home.html')
 
+##################################################################### CONVENIOS
+@login_required(login_url='/login/')
 def convenios(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/convenios.html')
   else:
     return render(request, 'cliente/convenios.html')
 
+#################################################################### FINANCEIRO
+@login_required(login_url='/login/')
 def financeiro(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/financeiro.html')
   else:
     return render(request, 'cliente/financeiro.html')
 
+################################################################# NOVA CONSULTA
+@login_required(login_url='/login/')
 def novaConsulta(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/nova_consulta.html')
   else:
     return render(request, 'cliente/nova_consulta.html')
 
+#################################################################### NOVO EXAME
+@login_required(login_url='/login/')
 def novoExame(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/novo_exame.html')
@@ -35,30 +43,41 @@ def novoExame(request):
     return render(request, 'cliente/novo_exame.html')
 
 ################################################################## MINHAS GUIAS
+
+################################################################### SOLICITADAS
+@login_required(login_url='/login/')
 def mgSolicitadas(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/mg1-solicitadas.html')
   else:
     return render(request, 'cliente/mg1-solicitadas.html')
 
+##################################################################### APROVADAS
+@login_required(login_url='/login/')
 def mgAprovadas(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/mg2-aprovadas.html')
   else:
     return render(request, 'cliente/mg2-aprovadas.html')
 
+##################################################################### FATURADAS
+@login_required(login_url='/login/')
 def mgFaturadas(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/mg3-faturadas.html')
   else:
     return render(request, 'cliente/mg3-faturadas.html')
 
+##################################################################### AUDITADAS
+@login_required(login_url='/login/')
 def mgAuditadas(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/mg4-auditadas.html')
   else:
     return render(request, 'cliente/mg4-auditadas.html')
 
+#################################################################### CANCELADAS
+@login_required(login_url='/login/')
 def mgCanceladas(request):
   if request.device['is_mobile']:
     return render(request, 'cliente-mob/mg5-canceladas.html')
